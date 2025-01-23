aa391bb06291465bfe36357d692f1ec7a3abff80
shengxic <124843788+SeanXC@users.noreply.github.com>
2025-01-23 16:22:46 +0000

    search bar + who's that pokemon + front simplification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,41 +3,22 @@ import Header from './components/Header';
 import Pokedex from './pages/Pokedex';
 import WhosThatPokemon from './pages/WhosThatPokemon';
 import MyAccount from './pages/MyAccount';
-import { Pokemon } from './types/Pokemon';
-import './App.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Pokedex');
 
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([
-    { id: 1, name: 'Bulbasaur', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png', types: ['Grass', 'Poison'], isFavorite: false },
-    { id: 4, name: 'Charmander', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png', types: ['Fire'], isFavorite: false },
-    { id: 25, name: 'Pikachu', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', types: ['Electric'], isFavorite: false },
-  ]);
-
-  const toggleFavorite = (id: number) => {
-    setPokemonList((prevList) =>
-      prevList.map((pokemon) =>
-        pokemon.id === id ? { ...pokemon, isFavorite: !pokemon.isFavorite } : pokemon
-      )
-    );
-  };
-
   const renderPage = () => {
     switch (currentPage) {
       case 'Pokedex':
-        return <Pokedex pokemons={pokemonList} onToggleFavorite={toggleFavorite} />;
+        return <Pokedex />;
       case 'WhosThatPokemon':
         return <WhosThatPokemon />;
       case 'MyAccount':
-        return (
-          <MyAccount
-            favorites={pokemonList.filter((pokemon) => pokemon.isFavorite)}
-            onToggleFavorite={toggleFavorite} 
-          />
-        );
+        return <MyAccount />;
       default:
-        return <Pokedex pokemons={pokemonList} onToggleFavorite={toggleFavorite} />;
+        return <Pokedex />;
     }
   };
 
@@ -49,5 +30,3 @@ function App() {
 }
 
 export default App;
-
-
--- frontend/src/pages/WhosThatPokemon.tsx
+++ frontend/src/pages/WhosThatPokemon.tsx
@@ -1,27 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import loadingIcon from '../assets/loading.svg';
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+import { Pokemon } from '../types/Pokemon';
 
 const WhosThatPokemon: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false); 
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  const pokemon: Pokemon =
+    { id: 1, name: 'Bulbasaur', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png', types: ['Grass', 'Poison'], isFavorite: false }; 
+  
   return (
-    <div>
-      {loading ? (
-        <div className="loading">
-          <img src={loadingIcon} alt="Loading" className="loading-icon" />
-        </div>
-      ) : (
-        <h1></h1>
-      )}
-    </div>
+    <>
+      <div className='center margin'>
+        <img src={pokemon.imageUrl} width="180px" />
+      </div>
+      <SearchBar placeHolder="Who's the name of that pokemon?" onSearch={()=>{}} />
+    </>
   );
 };
 
